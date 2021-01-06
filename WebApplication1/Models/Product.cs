804534017beee34752fcf3d95d60dde7b5c9da83
zFurkan <44337465+zFurkan@users.noreply.github.com>
2021-01-06 23:57:28 +0300

    create view

--- WebApplication1/Models/Product.cs
+++ WebApplication1/Models/Product.cs
@@ -12,6 +12,7 @@ namespace WebApplication1.Models
         public string Description { get; set; }
         public decimal Price { get; set; }
         public string Category { get; set; }
+        
 
     }
 }
