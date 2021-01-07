449d4e291ed6335aeab1eca54d43a18f2457e9e0
zFurkan <44337465+zFurkan@users.noreply.github.com>
2021-01-07 03:15:52 +0300

    Authentication

---
+++ WebApplication1/Models/WebUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class WebUser:IdentityUser
+    {
+        [PersonalData]
+        public string Name { get; set; }
+        [PersonalData]
+        public DateTime DOB { get; set; }
+    }
+}
--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -31,7 +31,7 @@ namespace WebApplication1
             services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection3")));
            
 
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<WebUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
